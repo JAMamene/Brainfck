12934a65c6f71b234cfb7de1c058e4380efa44ef
JulienMaureille <julien.maureille@live.fr>
2016-12-04 18:22:19 +0100

    Gestion des procedures

---
+++ src/level2/procedures/IProcedure.java
@@ -0,0 +1,16 @@
+package level2.procedures;
+
+import level2.constants.Executable;
+
+import java.util.List;
+
+/**
+ * Created on 04/12/2016.
+ * Polytech'Nice Sophia - SI3
+ *
+ * @author Julien Maureille
+ */
+public interface IProcedure {
+    void declare(String name, List<Executable> body);
+    List<Executable> getBody(String name);
+}
---
+++ src/level2/procedures/Procedure.java
@@ -0,0 +1,26 @@
+package level2.procedures;
+
+import level2.constants.Executable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class modelizing a procedure
+ */
+public class Procedure implements IProcedure {
+    private Map<String, List<Executable>> procedures = new HashMap<>();
+
+    /**
+     * Method that declares a procedure
+     * @param name the name of the procedure declared
+     * @param body the list of instructions
+     */
+    public void declare(String name, List<Executable> body){
+        procedures.put(name,body);
+    }
+
+    public List<Executable> getBody(String name) {
+        return procedures.get(name);
+    }
+}
