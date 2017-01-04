b11ea46265e05436b37c88b5a094fbf7c6742454
Quentin Sautel <quentin.sauel@etu.unice.fr>
2017-01-04 16:48:20 +0100

    Test "addshortcut" "getshortcut" (procédures, fonctions)

--- src/test/java/unitary/ShortcutTest.java
+++ src/test/java/unitary/ShortcutTest.java
@@ -1,11 +1,31 @@
 package unitary;
+import level4.instructions.Executable;
+import level4.instructions.InstructionEnum;
+import level4.reader.parser.Shortcut;
+import level4.utils.Procedure;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShortcutTest {
+    Shortcut shortcut;
+
+    @Before
+    public void init(){
+        shortcut= new Shortcut();
+    }
     @Test
-    public void ShortcutTest(){
+    public void ShortcutTest1(){
+        List<Executable> instrucut= new ArrayList<>();
+        instrucut.add(InstructionEnum.INCR);
+        Procedure proc=new Procedure(instrucut);
+        shortcut.addProcedure("ZERO",proc);
+        assertTrue(shortcut.getExecutable("ZERO[]") instanceof Procedure);
+
 
     }
 
