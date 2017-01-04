8ec315e51489907c599aec3ddf86b960b1a95d6d
Quentin Sautel <quentin.sauel@etu.unice.fr>
2017-01-04 17:01:14 +0100

    test shorcut procedure only, (not function) + improvement of ShortcutTest1

--- src/test/java/unitary/ShortcutTest.java
+++ src/test/java/unitary/ShortcutTest.java
@@ -1,4 +1,5 @@
 package unitary;
+
 import level4.instructions.Executable;
 import level4.instructions.InstructionEnum;
 import level4.reader.parser.Shortcut;
@@ -7,7 +8,6 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,19 +15,16 @@ public class ShortcutTest {
     Shortcut shortcut;
 
     @Before
-    public void init(){
-        shortcut= new Shortcut();
+    public void init() {
+        shortcut = new Shortcut();
     }
+
     @Test
-    public void ShortcutTest1(){
-        List<Executable> instrucut= new ArrayList<>();
+    public void ShortcutTest1() {
+        List<Executable> instrucut = new ArrayList<>();
         instrucut.add(InstructionEnum.INCR);
-        Procedure proc=new Procedure(instrucut);
-        shortcut.addProcedure("ZERO",proc);
+        Procedure proc = new Procedure(instrucut);
+        shortcut.addProcedure("ZERO", proc);
         assertTrue(shortcut.getExecutable("ZERO[]") instanceof Procedure);
-
-
     }
-
-
 }
